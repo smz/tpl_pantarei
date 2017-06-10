5bf5028dc32eb480f474cf263cad1e8609531c2e
Sergio Manzi <smz@smz.it>
2017-06-10 18:30:15 +0200

    Made viewport scalable

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ include_once __DIR__ . '/helpers/tpl-init.php';
 <html class='no-js <?php echo $bootstrapVersion; ?>' <?php echo $htmlOptions; ?>>
 
 <head>
-	<meta name='viewport' content='width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' />
+	<meta name='viewport' content='width=device-width, initial-scale=1.0' />
 <jdoc:include type="head" />
 <?php echo $customStyleSheet; ?>
 </head>
